d5521e00a68238c61ca2fd4ca2c2bcc569625007
ouiam-khattach <45097285+ouiam-khattach@users.noreply.github.com>
2020-09-27 23:25:57 +0100

    consume aws api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends Component {
+    state = {
+      todos: []
+    }
+    componentDidMount() {
+      fetch('https://9mz9ypoe2d.execute-api.eu-west-1.amazonaws.com/test/todolist?userId=2&id=2&title=tache1&completed=true')
+      
+      .then(res => res.json())
+      .then((data) => {
+        console.log("test")
+        console.log(data)
+        console.log(typeof data)
+        
+        console.log(typeof this.state.todos)
+        this.setState({ todos: data })
+        
+        console.log(this.state.todos)
+      })
+      .catch(console.log)
+    }
+  
+    render() {
 
-export default App;
+      return (
+         <div className="container">
+          <div className="col-xs-12">
+          <h1>My Todos</h1>
+         
+            <div className="card">
+              <div className="card-body">
+
+                <h5 className="card-title"> userId: {this.state.todos.userId}</h5>
+                <h5 className="card-title">id: {this.state.todos.id}</h5>
+                <h5 className="card-title">title: {this.state.todos.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                { this.state.todos.completed &&
+                  <span>
+                  Completed
+                  </span>
+                }
+                { !this.state.todos.completed &&
+                  <span>
+                    Pending
+                  </span>
+                }              
+                </h6>
+              </div>
+            </div>
+          
+          </div>
+         </div>
+      );
+    
+  }}
+    
+
+  export default App;
+
